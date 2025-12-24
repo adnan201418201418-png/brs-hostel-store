af01926a4da28698c06500e6c05893a387365029
adnan201418201418-png <adnan201418201418@gmail.com>
2025-12-24 11:09:30 +0530

    Add products data to data.js

---
+++ js/data.js
@@ -0,0 +1,5 @@
+const products = [
+  {id:1, name:"Pen", price:10, category:"Stationery", stock:10},
+  {id:2, name:"Notebook", price:50, category:"Stationery", stock:0},
+  {id:3, name:"Chips", price:20, category:"Snacks", stock:5}
+];
